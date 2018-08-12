ec4fdcb1bf502c8b762e4bdc72a7f003d268da47
jgi <public-vs2015@gissehel.org>
2018-08-12 11:10:45 +0200

    REFACT : Use Wox.EasyHelper

--- Wox.UnitConverter/UnitConverterPlugin.cs
+++ Wox.UnitConverter/UnitConverterPlugin.cs
@@ -5,155 +5,19 @@ using System.Text;
 using Unit.Lib.Core.DomainModel;
 using Unit.Lib.Core.Service;
 using Unit.Lib.Service;
+using Wox.EasyHelper;
 using Wox.Plugin;
+using Wox.UnitConverter.Service;
 
 namespace Wox.UnitConverter
 {
-    public class UnitConverterPlugin : IPlugin
+    public class UnitConverterPlugin : PluginBase<WoxUnitResultFinder>
     {
-        private IUnitService<ScalarFloat, float> UnitService { get; set; }
-
-        public void Init(PluginInitContext context)
+        public override WoxUnitResultFinder PrepareContext()
         {
             var constantProvider = new ConstantProvider<ScalarFloat, float>();
-            UnitService = new UnitService<ScalarFloat, float>(constantProvider);
-        }
-
-        private Tuple<string, string> Separate(string value, string pattern)
-        {
-            var patternIndex = value.IndexOf(pattern);
-            if (patternIndex < 0)
-            {
-                return null;
-            }
-            var left = value.Substring(0, patternIndex);
-            var right = value.Substring(patternIndex + pattern.Length, value.Length - (patternIndex + pattern.Length));
-            return new Tuple<string, string>(left, right);
-        }
-
-        public List<Result> Query(Query query)
-        {
-            string title = query.Search;
-            string output = null;
-
-            try
-            {
-                var search = query.Search;
-                var unitString = search;
-                string targetUnitString = null;
-                string targetExtraUnitString = null;
-                var arrowFields = Separate(search, "->");
-                if (arrowFields != null)
-                {
-                    unitString = arrowFields.Item1;
-                    targetUnitString = arrowFields.Item2;
-                }
-
-                if (targetUnitString != null)
-                {
-                    var doubleComaFields = Separate(targetUnitString, ":");
-                    if (doubleComaFields != null)
-                    {
-                        targetUnitString = doubleComaFields.Item1;
-                        targetExtraUnitString = doubleComaFields.Item2;
-                    }
-                }
-                var unit = UnitService.Parse(unitString);
-                string inputUnit = unit.UnitElement.Name;
-                string outputUnit = null;
-
-                if (targetUnitString != null)
-                {
-                    var targetUnit = UnitService.Parse(targetUnitString);
-                    UnitValue<ScalarFloat, float> targetExtraUnit = null;
-                    if (targetExtraUnitString != null)
-                    {
-                        targetExtraUnit = UnitService.Parse(targetExtraUnitString);
-                    }
-                    var result = UnitService.Convert(UnitService.Divide(unit, targetUnit));
-                    string resultAsString = null;
-                    string targetUnitAsString = null;
-
-                    if (result.UnitElement.GetDimension().QuantityCount == 0)
-                    {
-                        resultAsString = result.Value.Value.ToString();
-                    }
-                    else
-                    {
-                        if (targetExtraUnit != null)
-                        {
-                            result = UnitService.Convert(result, targetExtraUnit);
-                        }
-                        outputUnit = result.UnitElement.Name;
-                        resultAsString = string.Format("({0})", result.AsString);
-                    }
-
-                    if (targetUnit.Value.Value == targetUnit.Value.GetNeutral().Value)
-                    {
-                        targetUnitAsString = targetUnit.UnitElement.AsString;
-                    }
-                    else
-                    {
-                        targetUnitAsString = string.Format("({0})", targetUnit.AsString);
-                    }
-                    if (targetUnit.UnitElement.GetDimension().QuantityCount != 0)
-                    {
-                        if (outputUnit == null)
-                        {
-                            outputUnit = targetUnit.UnitElement.Name;
-                        }
-                        else
-                        {
-                            outputUnit = outputUnit + " x " + targetUnit.UnitElement.Name;
-                        }
-                    }
-                    output = string.Format("{0} {1}", resultAsString, targetUnitAsString);
-                }
-                else
-                {
-                    var result = UnitService.Convert(unit);
-                    if (result.UnitElement.GetDimension().QuantityCount == 0 && result.Value.Value == 1)
-                    {
-                        output = null;
-                    }
-                    else
-                    {
-                        output = result.AsString;
-                        if (result.UnitElement.GetDimension().QuantityCount != 0)
-                        {
-                            outputUnit = result.UnitElement.Name;
-                        }
-                    }
-                }
-                if (outputUnit != null)
-                {
-                    title = string.Format("{0} ( {1} -> {2} )", title, inputUnit, outputUnit);
-                }
-                else
-                {
-                    title = string.Format("{0} ( {1} -> )", title, inputUnit);
-                }
-            }
-            catch (Exception ex)
-            {
-                title = "Exception !";
-                output = ex.Message;
-            }
-
-            if (output == null)
-            {
-                return null;
-            }
-
-            return new List<Result>
-            {
-                new Result
-                {
-                    Title = title,
-                    SubTitle = output,
-                    IcoPath = "Unitlib-64.png",
-                }
-            };
+            IUnitService<ScalarFloat, float> unitService = new UnitService<ScalarFloat, float>(constantProvider);
+            return new WoxUnitResultFinder(WoxContextService, unitService);
         }
     }
 }
